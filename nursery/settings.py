b21130dac8ff02ef69d3f176e448a4b2784c591a
dharmawebservice <dharmawebservice@gmail.com>
2026-06-02 12:07:05 +0530

    Test context processor

--- nursery/settings.py
+++ nursery/settings.py
@@ -79,7 +79,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'web.context_processors.user_profile',
+                #'web.context_processors.user_profile',
             ],
         },
     },
