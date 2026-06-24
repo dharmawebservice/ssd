a0eae5f0d6a9222df3da07c6a6c13461fb17f7cd
dharmawebservice <dharmawebservice@gmail.com>
2026-06-24 18:27:13 +0530

    final update 7.6

--- nursery/settings.py
+++ nursery/settings.py
@@ -19,12 +19,15 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.getenv(
     "ALLOWED_HOSTS",
-    "localhost,127.0.0.1,ssd-524c.onrender.com"
+    "localhost,127.0.0.1,ssd-524c.onrender.com,ssdnursery.in,www.ssdnursery.in"
 ).split(",")
 
 CSRF_TRUSTED_ORIGINS = os.getenv(
     "CSRF_TRUSTED_ORIGINS",
     "http://127.0.0.1:8000,http://localhost:8000"
+    "https://ssdnursery.in",
+    "https://www.ssdnursery.in",
+    "https://ssd-524c.onrender.com",
 ).split(",")
 
 # ==================================================
